7add6cda8b655eaee89b9b9bfa1c7c6913413777
Suzie Wong <monkeysuzie@gmail.com>
2012-10-10 16:14:55 +0800

    finish all the php files without jQuery

--- cal.sql
+++ cal.sql
@@ -30,7 +30,7 @@ CREATE TABLE `cal` (
   `event_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`event_id`),
   KEY `event_start` (`event_start`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,8 +39,34 @@ CREATE TABLE `cal` (
 
 LOCK TABLES `cal` WRITE;
 /*!40000 ALTER TABLE `cal` DISABLE KEYS */;
-INSERT INTO `cal` VALUES (1,'New year','happy new year!','2009-12-31 16:00:00','2010-01-01 15:59:59'),(2,'Last Day of January','Last day of the mouth','2010-01-30 16:00:00','2010-01-31 15:59:59');
+INSERT INTO `cal` VALUES (1,'New year','happy new year!','2009-12-31 16:00:00','2010-01-01 15:59:59'),(2,'Last Day of January','Last day of the mouth','2010-01-30 16:00:00','2010-01-31 15:59:59'),(7,'Party&#26202;&#20250;','&#20170;&#22825;&#35201;&#21507;&#26202;&#39277;,&#20320;&#24320;&#19981;&#24320;&#26032;','2010-01-22 09:00:00','2010-01-22 11:00:00'),(8,'&#26089;&#39277;','&#22909;&#21543;&#65292;&#25105;&#25215;&#35748;&#25105;&#36755;&#20102;','2010-01-21 09:00:00','2010-01-21 11:00:00'),(10,'&#19978;&#35838;','&#19978;&#20844;&#36873;&#35838;','2010-02-28 23:00:00','2010-03-02 15:59:59');
 /*!40000 ALTER TABLE `cal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(80) DEFAULT NULL,
+  `user_pass` varchar(47) DEFAULT NULL,
+  `user_email` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','6d9f2d190855fbb68caaf4b765596b44771759f967f15ea','admin@admin.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-09-22 17:24:50
+-- Dump completed on 2012-10-10 16:13:39
